36b76138e513392e1137a7f58c5ed4560bc8fd1a
bsc-com-29-20 <bsc-com-29-20@unima.ac.mw>
2024-10-28 09:49:25 +0200

    Landing Page

---
+++ rc_front/src/App.jsx
@@ -0,0 +1,18 @@
+// // import { useState } git from 'react'
+// // import reactLogo from './assets/react.svg'
+// // import viteLogo from '/vite.svg'
+// import './App.css'
+
+import Landing from "./components/LandingPage"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Landing/>
+    </>
+  )
+}
+
+export default App
